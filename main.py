0634a696061f28954a99764289237bbc234842b4
Alison Keenan <apk2150@barnard.edu>
2023-11-29 22:43:39 -0500

    prints csv

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ results = soup.find(id="page-content")
 all_schools = results.find_all("td", class_="schoolname")
 # print(results.prettify())
 for school in all_schools:
-    print(school, end="\n" * 2)
+    # print(school, end="\n" * 2)
     link=school.find("a")
     school_name= link.text
     school_roster_url="https://scores.collegesailing.org"+link["href"]+SEASON+"/roster/"
-    print(school_roster_url)
-    # new_page = requests.get(URL)
-    # new_soup = BeautifulSoup(page.content, "html.parser")
-    # links = new_soup.find_all('a', text='text to find')
+    # print(school_roster_url)
+    new_page = requests.get(school_roster_url)
+    new_soup = BeautifulSoup(new_page.content, "html.parser")
+    header=new_soup.find(id="content-header")
+    roster_size = header.find_all('span', class_="page-info-value")
+    try:
+        print(school_name+","+roster_size[-1].text)
+    except:
+        print(school_name + ",")
+
