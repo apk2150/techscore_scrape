9338f30f2446a2035f5bb9038d39afc8c4ba8f45
Alison Keenan <apk2150@barnard.edu>
2023-11-29 22:24:28 -0500

    got roster pages urls for each school

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-
+#find number of students registered on each team
 import requests
 from bs4 import BeautifulSoup
 #https://realpython.com/beautiful-soup-web-scraper-python/
-
+SEASON="f23"
 URL = "https://scores.collegesailing.org/schools/"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="page-content")
-job_elements = results.find_all("td", class_="schoolname")
+all_schools = results.find_all("td", class_="schoolname")
 # print(results.prettify())
-for job_element in job_elements:
-    print(job_element, end="\n"*2)
+for school in all_schools:
+    print(school, end="\n" * 2)
+    link=school.find("a")
+    school_name= link.text
+    school_roster_url="https://scores.collegesailing.org"+link["href"]+SEASON+"/roster/"
+    print(school_roster_url)
+    # new_page = requests.get(URL)
+    # new_soup = BeautifulSoup(page.content, "html.parser")
+    # links = new_soup.find_all('a', text='text to find')
